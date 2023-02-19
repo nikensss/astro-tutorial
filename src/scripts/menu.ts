bcd71dad423db24e46b8ddb78833cabcf444d5cf
Ricard Mallafre <rikitzzz@gmail.com>
2023-02-19 19:03:04 +0100

    feat: hamburger menu with interaction

---
+++ src/scripts/menu.ts
@@ -0,0 +1,7 @@
+export const addEventsListeners = () => {
+  document.querySelector('.hamburger')?.addEventListener('click', () => {
+    document.querySelector('.nav-links')?.classList.toggle('expanded');
+  });
+}
+
+addEventsListeners();
